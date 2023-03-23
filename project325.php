5b690de94b8e5935883dbe9e29608bc51995d8da
Anandmod <128707163+Anandmod@users.noreply.github.com>
2023-03-24 00:52:45 +0530

    myproject

---
+++ project325.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en"  dir="ltr">
+<head>
+<meta charset="utf-8">
+<title>Charity club</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="proj1.css">
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+<style type="text/css">
+ a {
+  text-align: center;
+  cursor: pointer;
+
+}
+a.active, a:hover{
+	background-color:red;
+	transition:0.5s;
+}
+
+ a:hover {
+  background-color: red;
+} 
+
+</style>
+</head>
+<body>
+<nav>
+<input type="checkbox" id="check">
+<label for="check" class="checkbtn">
+<i class="fas fa-bars"></i>
+</label>
+<label class="logo"><span style="color:white; font-family:Cursive; ">ChARiTY</span><span style="color:red; font-family:Cursive; padding: 1.5px;" >cLuB</span>
+</label>
+<ul>
+<li><a class="active" href="project325.php">HOME</a></li>
+<li><a href="aboutf.php">ABOUT</a></li>
+<li><a href="form.php">SIGN UP</a></li>
+<li><a href="lgn.php">LOGIN</a></li>
+<li><a href="donate1.php">DONATE</a></li>
+</ul>
+</nav>
+<div>
+<div class="container">
+	<img src="charity.jpg" style="width:98%; margin-right:20px; padding:100px; ">
+</div>
+
+<div class="centered" ><h3 style="color:white; 
+        font-size: 40px; margin-top:-800px; margin-left:35%; font-family:Cursive;"> Alone we can do so little</h3></br> <h2 style="color:red; 
+        font-size: 30px;  margin-left:35%; font-family:Cursive;">Together we can do so much.<h2></br>
+			<button class="button" style="width:100px; height:30px;
+			 background-color:red; margin-left:46%; margin-top:-70px; border-radius:8px; padding:1.5px; color:white;"><a  style="color:white;" href="donate1.php">DONATE</a></button>
+</div>
+</div>
+</br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
+<div style="background-color:#2c3e50; width: 240px; height:35px; border-radius:8px; margin-left:40%;">
+    <h2 style="text-align:center; margin-top:10px; color:red;  font-family:Cursive; padding: 1.5px;"> How Do We Work </h2></div>
+ 
+<div class="hh" style="display:flex;">
+<div>
+<img src="wd5.jpeg"  style="width:430px; height:330px;"> 
+<h4 style="text-align: center;  color:red;  font-size: 20px; font-family:Cursive;">Schedule a Pickup</h4>
+<p style="color:#2c3e50;  font-family:Cursive; font-size: 16px; margin:20px;">Enter the pickup location, and schedule a pickup.</br>
+Schedule-a-pickup service allows you to register a pickup online for all services that require a door pickup.</p>
+</div>
+<div>
+<img src="wdn.jpeg" style="width:430px; height:330px">
+<h4 style="text-align: center;  color:red;  font-size: 20px; font-family:Cursive;">Collect at your Doorstep</h4>
+<p style="color:#2c3e50;  font-family:Cursive; font-size: 16px;  margin:20px;">We will come to your doorstep to pick up the donations in the chosen slot and deliver them to the poor people where they can be given a new life.</p>
+</div>
+<div>
+<img src="d3.jpg" style="width:430px; height:330px;" >
+<h4 style="text-align: center;  color:red; font-size: 20px; font-family:Cursive;">Serve to poor people</h4>
+<p style="color:#2c3e50; font-family:Cursive; font-size: 16px;  margin:20px;">One of the better ways to help the poor and 
+needy people is to give them a hand up rather than a hand out.</p>
+</div>
+	
+</div>
+</br>
+
+<div style="background-color:#2c3e50; width: 240px; height:35px; border-radius:8px; margin-left:40%;">
+    <h2 style="text-align:center; margin-top:10px; color:red; font-family:Cursive; padding: 1.5px;"> Donate AnyThing </h2></div></br>
+</br>
+<div class="hh" style="display:flex;">
+<div>
+<img src="d5.jpeg"  style="width:430px; height:330px;"> 
+</div>
+<div>
+<img src="wp1.jpeg" style="width:430px; height:330px">
+</div>
+<div>
+<img src="a.png" style="width:430px; height:330px;">
+</div>
+</div>
+
+<div class="hh1" style="display:flex; width:100%;">
+<div>
+<img src="d6.jpeg"  style="width:430px; margin:100px;  height:330px;"> 
+</div>
+<div>
+<img src="d8.jpg" style="width:430px; margin:80px; margin-right:10px; height:330px">
+</div>
+</div>
+		
+		<!--HTML footer tag starts here-->
+		<footer>
+		    <div style="display:flex; width:100%; background-color:#2c3e50;">
+			<div class="column" style="background-color:; width:33%; margin-left:100px; height:250px; "><h5 style=" margin:20px; color:black; font-size: 23px;">Site links </h5>
+				
+
+
+				<ul style="list-style-type:disc; color:black;" >
+					<li ><a class="home" href="project325.php" style="color:black; margin:20px; font-size: 20px;">Home</a></li>
+					<li ><a href="aboutf.php"  style="color:black; font-size: 20px;  margin:20px;">About</a></li>
+					<li ><a href="form.php"  style="color:black; font-size: 20px;  margin:20px;">Sign Up</a></li>
+					<li ><a href="lgn.php"  style="color:black; font-size: 20px;  margin:20px;">Login</a></li>
+					<li ><a href="donate1.php"  style="color:black; font-size: 20px;  margin:20px;">Donate</a></li>
+				</ul>
+	
+			</div>
+			 
+			
+			<div class="column" style="background-color:; width:33%; height:250px;"><h5 style=" margin:20px; color:black; font-size: 23px;"> Contact Us </h5>
+
+				<ul style="margin-left:20px;">
+					<li style="color:black; font-size: 20px;">anand@gmail.com </li>
+					<li style="color:black; font-size: 20px;">7310106061</li>
+					<li  style="color:black; font-size:20px;">abhishek@gmail.com</li>
+					<li style="color:black; font-size: 20px;">7480860225</li>
+					
+				</ul>
+		
+		
+</div>
+<div style="display:inline; width:34%; height:250px; "> <h5 style=" margin:20px; margin-left:0px; color:black; font-size: 23px;"> Follow Us </h5>
+<div>
+		
+		<a href="www.facebook.com" style="cursor:pointer;"><img src="fl.png" style="width:30px; height:20px;"></a>
+		</div>
+		
+<div>
+		
+		<a href="www.instagram.com" style="cursor:pointer;"><img src="il.jpg" style="width:30px; height:20px;" ><a>
+		</div>
+		
+
+<div>
+		
+		<a href="www.linkedin.com" style="cursor:pointer;"><img src="ll.png" style="width:30px; height:20px;" ></a>
+		</div>
+		
+
+<div>
+		
+		<a href="www.twiter.com" style="cursor:pointer; hover:red;"><img src="tl1.png" style="width:30px; height:20px;" ></a>
+		</div>
+	
+		 
+		
+</div>
+</div>
+		</footer>
+		<div class="footer" style="text-align:center; background-color:#2c3e50;">  
+           <p style="font-size:20px;">@Copyright <span style="color:white;">ChARiTY</span><span style="color:red;" >cLuB</span>  2022- All Right Reserved.</p>
+</div>
+</body>
+</html>
